bdf26c84fa44e711b7c84c74314e4e350249d50c
Alejandro_Osendi <alejandro.osendi@doubledot.es>
2022-04-17 02:10:33 +0200

    First Commit

---
+++ extend.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Runig006\InvertSlug;
+
+
+use Flarum\Extend;
+use s9e\TextFormatter\Configurator;
+
+return [
+    (new Extend\Formatter)
+        ->configure(function (Configurator $config) {
+            $config->MediaEmbed->defaultSites->delete('youtube');
+            $config->MediaEmbed->add(
+                'youtube',
+                [
+                    'host'    => ['youtube.com', 'youtu.be'],
+                    'extract' => [
+                        "!youtube\\.com/watch\\?v=(?'id'[-0-9A-Z_a-z]+)!",
+                        "!youtu\\.be/(?'id'[-0-9A-Z_a-z]+)!"
+                    ],
+                    'iframe'  => [
+                        'width'  => 560,
+                        'height' => 315,
+                        'src'    => 'https://piped.kavin.rocks/embed/{@id}?'
+                    ]
+                ]
+            );
+        })
+];
